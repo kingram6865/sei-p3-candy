3f6fb5e7af15fd9bc2119fbaf5b025f27b9c9e3d
Kamil Keawe <kamilkeawe@Kamils-MacBook-Pro.local>
2020-09-24 16:09:17 -0600

    Backend to Develop

---
+++ modals/candy.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Candy = new Schema(
+  {
+    name: { type: String, required: true },
+    price: { type: String, required: true },
+    imgURL: { type: String, required: true },
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('candies', Candy)
