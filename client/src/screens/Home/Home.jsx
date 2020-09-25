87e03ac8545c0ea2f24c1cd78d710ff806b584c0
Diane So <your_email_here>
2020-09-25 11:18:30 -0400

    importing Searchbar and in process of adding handlesubmit and handlechange

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Layout from "../../components/shared/Layout/Layout";
+import SearchBar from "../../components/SearchBar/SearchBar";
 
 const Home = () => {
+
+  const handleSubmit = e => insert - redirect - here;
+
   return (
     <Layout>
       <div>change to image-carousel</div>
-      <div>insert-search-bar-here</div>
+      <SearchBar onSubmit={handleSubmit} onChange={handleSearch} />
       <div>insert-displaycase-here</div>
     </Layout>
   )
