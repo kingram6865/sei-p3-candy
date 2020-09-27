2d91bd5d43398959a9cbc7c1543591f3859f85c3
Kamil Keawe <alexpaiaina@gmail.com>
2020-09-26 18:52:50 -0600

    backend to development

--- models/candy.js
+++ models/candy.js
@@ -8,6 +8,7 @@ const Candy = new Schema(
     imgURL1: { type: String, required: true },
     imgURL2: { type: String, required: true },
     imgURL3: { type: String, required: true },
+    description: { type: String, required: true },
     reviews: [{
       author: { type: String, required: true },
       rating: { type: String, required: true },
