f0a74fe51dbffdc8462cd8baa18986773670d68b
Ken Ingram <ken.ingram@gmail.com>
2020-09-27 19:54:58 -0700

    Redirect from CandyCreate and CandyEdit

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './screens/Home/Home';
 import CandyCreate from './screens/CandyCreate/CandyCreate'
 import CandyEdit from './screens/CandyEdit/CandyEdit'
+import Candies from './screens/Candies/Candies'
 
 import './App.css';
 
@@ -11,8 +12,9 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/candies/:id" component={Candies} />
         <Route path="/add-candy" component={CandyCreate} />
-        <Route exact path="/candies/:id" component={CandyEdit} />
+        <Route exact path="/candies/:id/edit" component={CandyEdit} />
       </Switch>
     </div>
   );
