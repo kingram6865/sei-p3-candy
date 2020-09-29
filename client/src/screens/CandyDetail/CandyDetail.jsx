5e9aa75785c2bfa5bfa3d2fcbd315ff3eb367d76
Diane So <so.dianeyj@gmail.com>
2020-09-29 01:03:30 -0400

    started reorganizing div containers

--- client/src/screens/CandyDetail/CandyDetail.jsx
+++ client/src/screens/CandyDetail/CandyDetail.jsx
@@ -24,37 +24,42 @@ const CandyDetail = (props) => {
 
   return (
     <Layout>
-      <div className="candy-detail">
-        <img
-          className="candy-detail-image"
-          src={candy.imgURL1}
-          alt={candy.productName}
-        />
-        <img
-          className="candy-detail-image"
-          src={candy.imgURL2}
-          alt={candy.productName}
-        />
-        <img
-          className="candy-detail-image"
-          src={candy.imgURL3}
-          alt={candy.productName}
-        />
-        <div className="detail">
-          <div className="name">{candy.productName}</div>
-          <div className="price">{`${candy.price}`}</div>
-          <div className="button-container">
-            <button className="edit-button">
-              <Link className="edit-link" to={`/candies/${candy._id}/edit`}>
-                Edit Item
-              </Link>
-            </button>
-            <button
-              className="delete-button"
-              onClick={() => deleteCandy(candy._id)}
-            >
-              Delete Item
-            </button>
+      <div className="candy-detail-container">
+        <div className="candy-detail">
+          <div className="candy-detail-img-container">
+            <img
+              className="candy-detail-image"
+              src={candy.imgURL1}
+              alt={candy.productName}
+            />
+            <img
+              className="candy-detail-image"
+              src={candy.imgURL2}
+              alt={candy.productName}
+            />
+            <img
+              className="candy-detail-image"
+              src={candy.imgURL3}
+              alt={candy.productName}
+            />
+          </div>
+
+          <div className="detail">
+            <div className="name">{candy.productName}</div>
+            <div className="price">{`${candy.price}`}</div>
+            <div className="button-container">
+              <button className="edit-button">
+                <Link className="edit-link" to={`/candies/${candy._id}/edit`}>
+                  Edit Item
+                </Link>
+              </button>
+              <button
+                className="delete-button"
+                onClick={() => deleteCandy(candy._id)}
+              >
+                Delete Item
+              </button>
+            </div>
           </div>
         </div>
       </div>
