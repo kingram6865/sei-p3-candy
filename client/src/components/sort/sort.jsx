f4d2d7e284fe24279baaba01169bf40f0a7b3e7b
Michael McMahon <michaelmcmahon@Mac-mini.fios-router.home>
2020-09-30 16:57:00 -0400

    Sorting Functionality

--- client/src/components/sort/sort.jsx
+++ client/src/components/sort/sort.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { getCandies } from '../../services/candies'
+import { AZ, ZA, lowestFirst, highestFirst } from '../../utils/sort'
 import './Sort.css'
 
 const Sort = (props) => {
+  const [setAllCandy] = useState([])
+  const [queriedCandy, setQueriedCandy] = useState([])
+  const [setSortType] = useState([])
+
+  useEffect(() => {
+    const fetchCandy = async () => {
+      const candy = await getCandies()
+      setAllCandy(candies)
+      setQueriedCandy(candies)
+    }
+    fetchCandy()
+  }, [])
+
+  const handleSort = type => {
+    setSortType(type)
+    switch (type) {
+      case 'name-ascending':
+        setQueriedCandy(AZ(queriedCandy))
+        break
+      case 'name-descending':
+        setQueriedCandy(ZA(queriedCandy))
+        break
+      case 'price-ascending':
+        setQueriedCandy(lowestFirst(queriedCandy))
+        break
+      case 'price-descending':
+        setQueriedCandy(highestFirst(queriedCandy))
+        break
+      default:
+        break
+    }
+  }
 
     const handleChange = (event) => {
         props.onChange(event.target.value)
