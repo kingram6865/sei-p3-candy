eeb9676dac2eb7732e6992f505a9c0e80318be87
Ken Ingram <ken.ingram@gmail.com>
2020-09-30 12:00:26 -0700

    Ironed out the search results finish with major help from Stefon.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './screens/Home/Home';
 import CandyCreate from './screens/CandyCreate/CandyCreate'
@@ -11,15 +11,32 @@ import SearchResults from './screens/SearchResults/SearchResults'
 import './App.css';
 
 function App() {
+  const [queryResults, setQueryResults] = useState([])
+  
+  const handleSearch = (item, arr) => {
+    const queryResult = arr.filter(candy => candy.productName.toLowerCase().includes(item.toLowerCase()))
+    return(queryResult)
+  }
+
+
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" component={Home} />
+        {/* <Route exact path="/" component={Home} /> */}
+        <Route exact path="/">
+          <Home handleSearch={handleSearch} setQueryResults={setQueryResults} />
+        </Route>
         <Route exact path="/candies" component={CandyManager} />
         <Route exact path="/candies/:id" component={CandyDetail} />
         <Route path="/add-candy" component={CandyCreate} />
         <Route exact path="/candies/:id/edit" component={CandyEdit} />
-        <Route exact path="/candies/" component={SearchResults} />
+        {/* <Route exact path="/search-candies" component={SearchResults} /> */}
+        <Route exact path="/search-candies" >
+          <SearchResults handleSearch={handleSearch} setQueryResults={setQueryResults} queryResults={queryResults}/>
+        </Route>
+
+
+
       </Switch>
     </div>
   );
--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Redirect } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { getCandies } from '../../services/candies'
-import handleSearch from '../../utils/search.js'
+// import handleSearch from '../../utils/search.js'
 import "./SearchBar.css";
 
 const SearchBar = (props) => {
-  const [queryResults, setQueryResults] = useState([])
+  
   const [inventory, setInventory] = useState([])
+  const history  = useHistory()
 
   useEffect(() => {
     const fetchCandy = async () => {
@@ -16,28 +17,10 @@ const SearchBar = (props) => {
     fetchCandy();    
   }, [])
 
-  useEffect(() => {
-    // const redirect = () => {
-    //   console.log(queryResults)
-    //   return <Redirect to={`/candies/}`} />
-    // }
-
-    // if (queryResults){
-    //   redirect()
-    // }
-    console.log(`[SearchBar: useEffect] ${queryResults.length}`)
-  },[queryResults])
-
-
-
   const handleSubmit = event => {
     event.preventDefault()
-    console.log(`[SearchBar Submit]: ${event.target.value}`)
-    // if (queryResults.length) {
-    //   return <Redirect to={`/candies/search-results/${queryResults}`} />
-    // } else {
-    //   console.log(`[SearchBar]: ${event.target.value}`)
-    // }
+    history.push("/search-candies")
+
   }
 
   return (
@@ -46,8 +29,7 @@ const SearchBar = (props) => {
         className="search-input"
         value={props.value}
         onChange={(e) => {
-          setQueryResults(handleSearch(e.target.value, inventory))
-          // console.log(handleSearch(e.target.value, inventory))
+          props.setQueryResults(props.handleSearch(e.target.value, inventory))
         }}
         name="search"
         placeholder="How Can Mama Help?"
