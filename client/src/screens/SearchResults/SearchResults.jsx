d29dbe5dd0bd33db44443c5917af0a2564da72e0
Ken Ingram <kingram6865@users.noreply.github.com>
2020-09-30 21:05:35 -0700

    Revert "Feature sort dropdown to development"

--- client/src/screens/SearchResults/SearchResults.jsx
+++ client/src/screens/SearchResults/SearchResults.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 // import { useParams } from "react-router-dom";
 import Layout from "../../components/shared/Layout/Layout";
 import Candy from "../../components/Candy/Candy"
-import Sort from '../../components/Sort/Sort'
-
 
 const SearchResults = (props) => {
   let productsJSX
@@ -35,19 +33,6 @@ const SearchResults = (props) => {
   //       description={results.description}
   //     />    
   // }
-  const candies = props.queriedCandy && props.queriedCandy.length > 0 ? props.queriedCandy : props.queryResults
-  const productsJSX = candies.map((candy, index) =>
-  <Candy
-    _id={candy._id}
-    productName={candy.productName}
-    price={candy.price}
-    imgURL1={candy.imgURL1}
-    imgURL2={candy.imgURL2}
-    imgURL3={candy.imgURL3}
-    description={candy.description}
-    key={index}
-  />
-  )
 
   productsJSX = props.queryResults.map((candy, index) =>
   <Candy
@@ -66,9 +51,7 @@ return (
     <>
       <Layout handleSearch={props.handleSearch} setQueryResults={props.setQueryResults}>
         <div className="results-container">
-        <h3>Search Results</h3>
-        <Sort handleSort={props.handleSort} setQueriedCandy={props.setQueriedCandy} />
-        {console.log(candies)}
+          <h3>Search Results</h3>
           { productsJSX }
         </div>
       </Layout>
