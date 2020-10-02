45114da1c36078ff4698ea4d999a54bde16c3034
Diane So <so.dianeyj@gmail.com>
2020-10-02 03:25:04 -0400

    css changes to the product manager page

--- client/src/screens/CandyCreate/CandyCreate.jsx
+++ client/src/screens/CandyCreate/CandyCreate.jsx
@@ -37,25 +37,28 @@ const CandyCreate = (props) => {
   return (
     <div className="create-candy-container">
       <div className="candy-create-title">Add New Product</div>
+      <div className="create-form-container">
         <form className="create-form" onSubmit={handleSubmit}>
+          <div className="create-edit-price">
+            <input
+              className="input-name"
+              name="productName"
+              placeholder="Product Name"
+              value={candy.productName}
+              required
+              onChange={handleChange}
+            />
+            <input
+              className="input-price"
+              name="price"
+              placeholder="Price"
+              value={candy.price}
+              required
+              onChange={handleChange}
+            />
+          </div>
           <input
-            className="input-name"
-            name="productName"
-            placeholder="Product Name"
-            value={candy.productName}
-            required
-            onChange={handleChange}
-          />
-          <input
-            className="input-price"
-            name="price"
-            placeholder="Price"
-            value={candy.price}
-            required
-            onChange={handleChange}
-          />
-          <input
-            className="input-image1"
+            className="create-input-img"
             name="imgURL1"
             placeholder="Main Image"
             value={candy.imgURL1}
@@ -63,7 +66,7 @@ const CandyCreate = (props) => {
             onChange={handleChange}
           />
           <input
-            className="input-image2"
+            className="create-input-img"
             name="imgURL2"
             placeholder="Additional Image 1"
             value={candy.imgURL2}
@@ -71,7 +74,7 @@ const CandyCreate = (props) => {
             onChange={handleChange}
           />
           <input
-            className="input-image3"
+            className="create-input-img"
             name="imgURL3"
             placeholder="Additional Image 2"
             value={candy.imgURL3}
@@ -87,11 +90,14 @@ const CandyCreate = (props) => {
             cols={78}
             onChange={handleChange}
           />
-          <button type="submit" className="submit-button">
-            Submit
-          </button>
+          <div className="create-submit-container">
+            <button type="submit" className="submit-button">
+              Save
+            </button>
+          </div>
         </form>
       </div>
+    </div>
   );
 };
 
